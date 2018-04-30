956dcda2d3a8e218c55431b175c3682451df25fa
IagoMendes <iagorainhamendes@hotmail.com>
2018-04-30 15:38:29 -0300

    limpando o codigo

--- FabioFerraoApp/app/src/main/java/desagil/ensino/grupo/pro/br/fabioferraoapp/SendAlert.java
+++ FabioFerraoApp/app/src/main/java/desagil/ensino/grupo/pro/br/fabioferraoapp/SendAlert.java
@@ -1,12 +1,12 @@
 package desagil.ensino.grupo.pro.br.fabioferraoapp;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.telephony.PhoneNumberUtils;
-import android.telephony.SmsManager;
-import android.view.View;
-import android.widget.Button;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.telephony.PhoneNumberUtils;
+        import android.telephony.SmsManager;
+        import android.view.View;
+        import android.widget.Button;
 
 public class SendAlert extends AppCompatActivity {
     @Override
@@ -25,17 +25,6 @@ public class SendAlert extends AppCompatActivity {
         buttonenviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mensagem_d.isEmpty()) {
-                    Utils.showToast(SendAlert.this, "Mensagem vazia!");
-                    return;
-                }
-
-                if (!PhoneNumberUtils.isGlobalPhoneNumber(numero_d)) {
-                    Utils.showToast(SendAlert.this, numero_d);
-                    Utils.showToast(SendAlert.this, "Telefone inválido!");
-                    return;
-                }
-
                 SmsManager manager = SmsManager.getDefault();
                 manager.sendTextMessage(numero_d, null, mensagem_d, null, null);
 
